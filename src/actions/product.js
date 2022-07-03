d82b0fb95b934a876447403c750c08cbc7f1133d
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2022-07-03 13:41:58 +0530

    product page redux setup

--- src/actions/product.js
+++ src/actions/product.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_PRODUCTS } from "./type";
+import { GET_PRODUCT, GET_PRODUCTS } from "./type";
 
 // Get Products
 export const getProducts = () => async (dispatch) => {
@@ -8,6 +8,21 @@ export const getProducts = () => async (dispatch) => {
 
     dispatch({
       type: GET_PRODUCTS,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProduct = (productid) => async (dispatch) => {
+  try {
+    const response = await axios.get(
+      `https://fakestoreapi.com/products/${productid}`
+    );
+
+    dispatch({
+      type: GET_PRODUCT,
       payload: response.data,
     });
   } catch (error) {
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,7 +1,27 @@
-import React from "react";
+import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getProduct } from "../../actions/product";
+import { useLocation } from "react-router-dom";
 
-function Product() {
-  return <div>Product</div>;
-}
+export const Product = ({ getProduct, products: { product, loading } }) => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const productId = pathname.substr(1, pathname.length);
+  useEffect(() => {
+    getProduct(productId);
+  }, [getProduct]);
 
-export default Product;
+  return <div>tttt</div>;
+};
+
+Product.propTypes = {
+  getProduct: PropTypes.func.isRequired,
+  products: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  products: state.products,
+});
+
+export default connect(mapStateToProps, { getProduct })(Product);
