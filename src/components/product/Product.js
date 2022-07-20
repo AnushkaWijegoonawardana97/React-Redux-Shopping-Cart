017042843cde61b535291fa654743a7dd27762d6
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2022-07-20 09:35:50 +0530

    product page ui & navigation menu edtis

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,18 +1,86 @@
 import PropTypes from "prop-types";
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import { getProduct } from "../../actions/product";
 import { useLocation } from "react-router-dom";
+import classes from "./Product.module.scss";
+import Navigation from "../ui/Navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export const Product = ({ getProduct, products: { product, loading } }) => {
   const location = useLocation();
   const pathname = location.pathname;
   const productId = pathname.substr(1, pathname.length);
+  console.log(product);
   useEffect(() => {
     getProduct(productId);
   }, [getProduct]);
 
-  return <div>tttt</div>;
+  return (
+    <Fragment>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <section className={classes.productpage_container}>
+          <Navigation />
+
+          <div className={classes.productpage_grid_container}>
+            <div
+              className={classes.productpage_image}
+              style={{ backgroundImage: `url(${product.image})` }}></div>
+
+            <div className={classes.productpage_description}>
+              <div>
+                <h1 className={classes.productpage_title}>{product.title}</h1>
+                <p className={classes.productpage_category}>
+                  {product.category}
+                </p>
+              </div>
+
+              <div className={classes.productpage_review_grid}>
+                <div className={classes.productpage_review_count}>
+                  {product.rating.count} Reviews
+                </div>
+
+                <div className={classes.productpage_review_rating_grid}>
+                  <div className={classes.productpage_review_average}>
+                    {product.rating.rate}
+                  </div>
+
+                  <div className={classes.productpage_review_stars}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                  </div>
+                </div>
+              </div>
+
+              <div className={classes.productpage_description_text}>
+                {product.description}
+              </div>
+
+              <div className={classes.productpage_prices}>
+                {product.price} $
+              </div>
+
+              <div className={classes.productpage_cta_buttons}>
+                <button className={classes.productpage_cart_button}>
+                  Add To Cart
+                </button>
+
+                <button className={classes.productpage_wishlist_button}>
+                  Add To Wist List
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </Fragment>
+  );
 };
 
 Product.propTypes = {
--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -3,12 +3,28 @@ import React from "react";
 import { connect } from "react-redux";
 import classes from "./Navigation.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartShopping,
+  faHeart,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 export const Navigation = (props) => {
   return (
     <div className={classes.navigation_menu}>
+      <Link to='/cart' className={classes.home}>
+        <span>
+          <FontAwesomeIcon icon={faHouse} />
+        </span>
+      </Link>
+
+      <Link to='/wishlist' className={classes.wish_list}>
+        <span>
+          <FontAwesomeIcon icon={faHeart} />
+        </span>
+      </Link>
+
       <Link to='/cart' className={classes.open_cart}>
         <span>
           <FontAwesomeIcon icon={faCartShopping} />
